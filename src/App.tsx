e0e15788ad3d0f0c1fa520039aaa3cdaed2314ed
Abhijeet Shyam Jagtap <abhijeet@rulezero.com>
2023-04-17 01:13:20 +0530

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import RegistrationWrapper from './pages/Registration/RegistrationWrapper'
+import ProfileWrapper from './pages/Profile/ProfileWrapper'
+import SplashWrapper from './pages/SplashScreen/SplashScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App h-screen font-viFont font-light'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<SplashWrapper />} />
+          <Route path='/registration' element={<RegistrationWrapper />} />
+          <Route path='/splash' element={<SplashWrapper />} />
+          <Route path='/profile' element={<ProfileWrapper />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/DesktopView.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function DesktopView() {
+  return <div>This site is built for mobile devices, please open in mobile view</div>
+}
+
+export default DesktopView
